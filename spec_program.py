725a2e979e6d03c84f329915514684e964c06ade
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-11-01 10:20:07 -0400

    More involved spec; takes longer to run.
    But reducing the search space (size of grid) helps.
    
    E.g.,
    python3 generate_sketch_grid.py 2 3 4 4 4 > /tmp/op.sk && time sketch
    --slv-parallel --bnd-inbits=2 --bnd-int-range=10 /tmp/op.sk
    
    finishes in ~4 seconds.
    
    However,
    python3 generate_sketch_grid.py 2 3 4 5 6 > /tmp/op.sk && time sketch
    --slv-parallel --bnd-inbits=2 --bnd-int-range=10 /tmp/op.sk
    
    still hadn't finished in 4 minutes.

--- spec_program.py
+++ spec_program.py
@@ -1,6 +1,9 @@
 spec_program = '''
 // Spec for Sketch
 |StateAndPacket| program (|StateAndPacket| state_and_packet) {
+  state_and_packet.pkt_0 = state_and_packet.pkt_0 + 1;
+  state_and_packet.pkt_1 = state_and_packet.pkt_0 + state_and_packet.pkt_1;
+  state_and_packet.state_0 = state_and_packet.state_0 + 1;
   return state_and_packet;
 }
 
