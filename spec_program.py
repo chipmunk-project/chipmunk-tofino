d17d88b27305e7428bfa4340cdb0327f909c022b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-11-04 03:28:37 +0000

    Expand spec; takes 1:45 secs on 56-core machine.
    Cmdline:
    
    python3 generate_sketch_grid.py 2 3 4 4 4 > /tmp/op.sk &&
    time sketch --slv-parallel --bnd-inbits=2 --bnd-int-range=10 /tmp/op.sk --fe-output-hole-func holes.txt

--- spec_program.py
+++ spec_program.py
@@ -4,6 +4,7 @@ spec_program = '''
   state_and_packet.pkt_0 = state_and_packet.pkt_0 + 1;
   state_and_packet.pkt_1 = state_and_packet.pkt_0 + state_and_packet.pkt_1;
   state_and_packet.state_0 = state_and_packet.state_0 + 1;
+  state_and_packet.state_1 = state_and_packet.state_0 + state_and_packet.state_1;
   return state_and_packet;
 }
 
