d7effd9f4de97ec10b5b6f045e746c31392c0e14
Anirudh Sivaraman <sk.anirudh@gmail.com>
2019-01-26 17:54:43 -0500

    Replace global vars with local vars in functions.
    This is useful for optimization verification because optverify assumes that there are only local variables.
    Unfortunately, slows down synthesis time.
    time python3 compile.py test.sk 3 3 now takes ~25 seconds instead of 10.

--- sketch_helpers.py
+++ sketch_helpers.py
@@ -42,7 +42,7 @@ def generate_stateless_alu(alu_name, potential_operands):
 # Updates the state in place and returns the old value of the state
 def generate_stateful_alu(alu_name):
   stateful_alu = '''
-int %s(ref int s, int y) {
+int %s(ref int s, int y, %s, %s, %s) {
   int opcode = %s;
   int immediate_operand = %s;
   int alu_mode = %s;
@@ -54,7 +54,9 @@ int %s(ref int s, int y) {
   }
   return old_val;
 }
-'''%(alu_name, alu_name + "_opcode", alu_name + "_immediate", alu_name + "_mode")
+'''%(alu_name,
+     "int " + alu_name + "_opcode_local", "int " + alu_name + "_immediate_local", "int " + alu_name + "_mode_local",
+     alu_name + "_opcode_local", alu_name + "_immediate_local", alu_name + "_mode_local")
   generate_hole(alu_name + "_opcode", 1)
   generate_hole(alu_name + "_immediate", 2)
   generate_hole(alu_name + "_mode", 1)
