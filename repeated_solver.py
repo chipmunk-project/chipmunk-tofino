7017defaf6ea28f3d92073d1dac4097827c654cf
XiangyuG <39160477+XiangyuG@users.noreply.github.com>
2019-03-08 23:13:28 +0800

    Add counter_example_generator to generate multiple cex (#33)
    * Add counter_example_generator to generate multiple cex
    
    * Update sol_verify.py
    
    Co-Authored-By: XiangyuG <39160477+XiangyuG@users.noreply.github.com>
    
    * Optimize and simplify the code in sol_verify.py
    
    * Modify counter_example_generator to add a file docstring

--- repeated_solver.py
+++ repeated_solver.py
@@ -1,5 +1,4 @@
 """Repeated Solver"""
-
 from pathlib import Path
 import re
 import subprocess
@@ -11,6 +10,7 @@ from jinja2 import Environment, FileSystemLoader, StrictUndefined
 from sketch_generator import SketchGenerator
 from utils import get_num_pkt_fields_and_state_groups
 from sol_verify import sol_verify
+from counter_example_generator import counter_example_generator
 
 if (len(sys.argv) != 8):  # This part may need change with the chipmunk.py file
     print(
@@ -74,8 +74,8 @@ else:
     with open("/tmp/" + sketch_name + "_result.holes", "w") as result_file:
         result_file.write(output)
     # Step2: run sol_verify.py
-    ret_code = sol_verify(sketch_name + "_codegen.sk" , "/tmp/"+ sketch_name +
-                          "_result.holes")
+    ret_code = sol_verify(sketch_name + "_codegen.sk",
+                          "/tmp/" + sketch_name + "_result.holes")
     if (ret_code == 0):
         print("success")
         end = time.time()
@@ -119,47 +119,51 @@ else:
 
                 # Add function assert here
                 if (count == 0):
-                    (ret_code_sketch_with_counter_example,
-                     output_with_counter_example) = subprocess.getstatusoutput(
-                         "sketch -V 3 --debug-cex --bnd-inbits=10 " +
-                         sketch_name + "_codegen_with_hole_value.sk")
+                    filename = sketch_name + "_codegen_with_hole_value.sk"
                 else:
-                    (ret_code_sketch_with_counter_example,
-                     output_with_counter_example) = subprocess.getstatusoutput(
-                         "sketch -V 3 --debug-cex --bnd-inbits=10 " + "/tmp/" +
-                         sketch_name + "_new_sketch_with_hole_value.sk")
-                pkt_group = re.findall("input (pkt_\d+)\w+ has value \d+= \((\d+)\)", output_with_counter_example)
-                state_group = re.findall("input (state_\d+)\w+ has value \d+= \((\d+)\)", output_with_counter_example)
-                print(pkt_group,"len= ", len(pkt_group))
-                print(state_group, "len= ", len(state_group))
-                counter_example_definition = "|StateAndPacket| x_" + str(count) + " = |StateAndPacket|(\n"
-                # Check if all packet fields are included in pkt_group as part of the counterexample.
-                # If not, set those packet fields to a default (0) since they don't matter for the counterexample.
-                for i in range(int(num_fields_in_prog)):
-                  if ("pkt_" + str(i) in [regex_match[0] for regex_match in pkt_group]):
-                    continue
-                  else:
-                    pkt_group.append(("pkt_" + str(i), str(0)))
+                    filename = "/tmp/" + sketch_name + "_new_sketch_with_hole_value.sk"
+                counter_example_assert = ""
+                counter_example_definition = ""
+                for bits in range(2,10):
+                    output_with_counter_example = counter_example_generator(bits,filename, num_fields_in_prog, num_state_vars)
+                    if (output_with_counter_example == ""):
+                        continue;
+                    else:
+                        pkt_group = re.findall("input (pkt_\d+)\w+ has value \d+= \((\d+)\)", output_with_counter_example)
+                        state_group = re.findall(
+                            "input (state_\d+)\w+ has value \d+= \((\d+)\)",
+                            output_with_counter_example)
+                        print(pkt_group, "len= ", len(pkt_group), "actual value", str(int(pkt_group[0][1]) + 2**bits))
+                        print(state_group, "len= ", len(state_group), "actual value", int(state_group[0][1]) + 2**bits)
+                        # Check if all packet fields are included in pkt_group as part of the counterexample.
+                        # If not, set those packet fields to a default (0) since they don't matter for the counterexample.
+                        for i in range(int(num_fields_in_prog)):
+                          if ("pkt_" + str(i) in [regex_match[0] for regex_match in pkt_group]):
+                            continue
+                        else:
+                          pkt_group.append(("pkt_" + str(i), str(0)))
 
-                # Check if all state vars are included in state_group as part of the counterexample.
-                # If not, set those state vars to a default (0) since they don't matter for the counterexample.
-                for i in range(int(num_state_vars)):
-                  if ("state_" + str(i) in [regex_match[0] for regex_match in state_group]):
-                    continue
-                  else:
-                    state_group.append(("state_" + str(i), str(0)))
+                        # Check if all state vars are included in state_group as part of the counterexample.
+                        # If not, set those state vars to a default (0) since they don't matter for the counterexample.
+                        for i in range(int(num_state_vars)):
+                          if ("state_" + str(i) in [regex_match[0] for regex_match in state_group]):
+                            continue
+                        else:
+                          state_group.append(("state_" + str(i), str(0)))
 
-                for i in range(len(pkt_group)):
-                  counter_example_definition += pkt_group[i][0] + " = " + pkt_group[i][1] + ','
-                for i in range(len(state_group)):
-                  if (i < len(state_group) - 1):
-                    counter_example_definition += state_group[i][0] + " = " + state_group[i][1] + ','
-                  else:
-                    counter_example_definition += state_group[i][0] + " = " + state_group[i][1] + ");\n"
-
-                counter_example_assert = "assert pipeline(" + "x_" + str(
-                    count) + ")" + " == " + "program(" + "x_" + str(
-                        count) + ");\n"
+                        counter_example_definition += "|StateAndPacket| x_" + str(
+                            count) + " = |StateAndPacket|(\n"
+                        for i in range(len(pkt_group)):
+                            counter_example_definition += pkt_group[i][
+                                0] + " = " + str(int(pkt_group[0][1]) + 2**bits) + ','
+                        for i in range(len(state_group) - 1):
+                            if (i < len(state_group) - 1):
+                              counter_example_definition += state_group[i][0] + " = " + state_group[i][1] + ','
+                            else:
+                              counter_example_definition += state_group[i][0] + " = " + state_group[i][1] + ");\n"
+                        counter_example_assert += "assert pipeline(" + "x_" + str(
+                            count) + "_" + str(bits) + ")" + " == " + "program(" + "x_" + str(
+                                count) + "_" + str(bits) + ");\n"
                 original_sketch_file_string = original_sketch_file_string[
                     0:
                     begin_pos] + counter_example_definition + counter_example_assert + original_sketch_file_string[
@@ -188,7 +192,8 @@ else:
                             0] + " = " + hits[0][1] + ";"
                 hole_value_file.write(hole_value_string)
                 hole_value_file.close()
-                ret_code = sol_verify("/tmp/" + sketch_name + "_new_sketch.sk" , "/tmp/"+ sketch_name +"_result.holes")
+                ret_code = sol_verify("/tmp/" + sketch_name + "_new_sketch.sk",
+                                      "/tmp/" + sketch_name + "_result.holes")
                 if (ret_code == 0):
                     print("finally succeed")
                     end = time.time()
--- sol_verify.py
+++ sol_verify.py
@@ -2,31 +2,33 @@
 
 import sys
 import subprocess
-def sol_verify(original_sketch_file,hole_value_file):
 
-  original_sketch_file_string    = open(str(original_sketch_file),"r").read()
-  hole_value_file_string         = open(str(hole_value_file),"r").read()
+def sol_verify(original_sketch_file, hole_value_file):
 
-  #remove the hole definition in original_sketch_file
-  original_sketch_file_string = original_sketch_file_string[original_sketch_file_string.rfind('??'):]
-  original_sketch_file_string = original_sketch_file_string[original_sketch_file_string.find(';')+1:]
+    original_sketch_file_string = open(str(original_sketch_file), "r").read()
+    hole_value_file_string = open(str(hole_value_file), "r").read()
 
-  #remove the redundant code in hole_value_file
-  begin_pos = hole_value_file_string.find('int')
-  end_pos = hole_value_file_string.rfind(';')
-  hole_value_file_string = hole_value_file_string[begin_pos:end_pos+1]
+    #remove the hole definition in original_sketch_file
+    original_sketch_file_string = original_sketch_file_string[
+        original_sketch_file_string.rfind('??'):]
+    original_sketch_file_string = original_sketch_file_string[
+        original_sketch_file_string.find(';') + 1:]
 
-  sketch_file_with_hole_value = hole_value_file_string + original_sketch_file_string
+    #remove the redundant code in hole_value_file
+    begin_pos = hole_value_file_string.find('int')
+    end_pos = hole_value_file_string.rfind(';')
+    hole_value_file_string = hole_value_file_string[begin_pos:end_pos + 1]
 
-  # Create file and write sketch_harness into it.
-  sketch_file = open(original_sketch_file[0:original_sketch_file.find('.')] + "_with_hole_value.sk", "w")
-  sketch_file.write(sketch_file_with_hole_value)
-  sketch_file.close()
+    sketch_file_with_hole_value = hole_value_file_string + original_sketch_file_string
 
-  # Call sketch on it
-  (ret_code, output) = subprocess.getstatusoutput("sketch --bnd-inbits=10 " + sketch_file.name)
-  if (ret_code!=0):
-    #fail print 1
-    return 1
-  else:
-    return 0
+    # Create file and write sketch_harness into it.
+    sketch_file = open(
+        original_sketch_file[0:original_sketch_file.find('.')] +
+        "_with_hole_value.sk", "w")
+    sketch_file.write(sketch_file_with_hole_value)
+    sketch_file.close()
+
+    # Call sketch on it
+    (ret_code, _) = subprocess.getstatusoutput("sketch --bnd-inbits=10 " +
+                                                    sketch_file.name)
+    return ret_code
